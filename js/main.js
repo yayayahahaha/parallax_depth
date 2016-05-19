acbb3a01a560037cbb8cd91b6f76d46cbe03f7b5
FlyC <yayayahahaha0011@yahoo.com.tw>
2016-05-19 22:06:33 +0800

    change scrollbar style and get rid of some problem of overflow

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
 window.onload = function(argument) {
-	try {
-		$();
-		console.log("The JQuery and javascript are running!");
-	} catch (str) {
-		console.error("The JQuery is NOT running! but javascript is running!!");
+	var parallax_array = document.getElementsByClassName('parallax');
+
+	for (var i = 0; i < parallax_array.length; i++) {
+		console.log(parallax_array[i].offsetTop);
 	}
-
-	$(".container").delay(500).animate({
-		scrollTop: 300},
-		300, function() {
-		console.log("animation done");
-	});;
-
-	$(".container").scroll(function(event) {
-		console.log(this.scrollTop);
-	});
-
 }
