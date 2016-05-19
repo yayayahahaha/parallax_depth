adbf0b8b5bccea0d49c28180fda51160c4031236
FlyC <yayayahahaha0011@yahoo.com.tw>
2016-05-19 21:03:17 +0800

    make basic scroll animation.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-window.onload=function (argument) {
-	try{
+window.onload = function(argument) {
+	try {
 		$();
 		console.log("The JQuery and javascript are running!");
-	}catch(str){
+	} catch (str) {
 		console.error("The JQuery is NOT running! but javascript is running!!");
 	}
+
+	$(".container").delay(500).animate({
+		scrollTop: 300},
+		300, function() {
+		console.log("animation done");
+	});;
+
+
 
 }
