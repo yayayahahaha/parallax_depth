5ae73c6b30a0bbcfb8ba27f537f5b50239decc4b
FlyC <yayayahahaha0011@yahoo.com.tw>
2016-05-19 23:04:33 +0800

    finished parallax with content

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(argument) {
-	var parallax_array = document.getElementsByClassName('parallax');
-
-	for (var i = 0; i < parallax_array.length; i++) {
-		console.log(parallax_array[i].offsetTop);
-	}
-}
